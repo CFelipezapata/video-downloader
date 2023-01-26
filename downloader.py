d880304c68c686cb85ef59656d14d8c57cd9c342
FZapata <97852383+felipezapata01@users.noreply.github.com>
2023-01-26 01:29:54 -0500

    Refactor to use functions instead

--- downloader.py
+++ downloader.py
@@ -6,8 +6,6 @@ from pytube import YouTube
 load_dotenv()
 
 STORE_PATH = os.getenv('VIDEO_STORE_PATH')
-video_link = sys.argv[1]
-file_name = sys.argv[2]
 
 # TODO: Complete functions for progress and completion to display a progress bar...
 def progress_func():
@@ -16,21 +14,30 @@ def progress_func():
 def complete_func():
     pass
 
-try:
-    yt = YouTube(video_link,
-                #  on_progress_callback=progress_func,
-                #  on_complete_callback=complete_func
-                 )
-except:
-    print('Connection Error')
-    
-video = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+def download_video(video_link: str, file_name: str) -> None:
+    try:
+        yt = YouTube(video_link,
+                    #  on_progress_callback=progress_func,
+                    #  on_complete_callback=complete_func
+                    )
+    except:
+        print('Connection Error')
+        
+    video = yt.streams.filter(file_extension='mp4').get_highest_resolution()
 
-try: 
-    # downloading the video 
-    video.download(STORE_PATH, f'{file_name}.mp4') 
-except Exception as e: 
-    print("There was an error downloading the video!", e) 
-print('Download completed successfully!') 
+    try: 
+        # downloading the video 
+        video.download(STORE_PATH, f'{file_name}.mp4') 
+    except Exception as e: 
+        print("There was an error downloading the video!", e) 
+    print('Download completed successfully!')
+
+def main() -> None:
+    video_link = sys.argv[1]
+    file_name = sys.argv[2]
+    download_video(video_link, file_name)
+
+if __name__ == '__main__':
+    main()
 
 
