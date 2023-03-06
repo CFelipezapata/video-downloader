3c04010c8a313665032d149a9781c678db116150
FZapata <97852383+felipezapata01@users.noreply.github.com>
2023-03-05 22:27:57 -0500

    Add support for audio downloads

--- downloader.py
+++ downloader.py
@@ -1,11 +1,18 @@
 import sys
 import os
+import argparse
 from dotenv import load_dotenv
 from pytube import YouTube
 
 load_dotenv()
 
-STORE_PATH = os.getenv('VIDEO_STORE_PATH')
+VIDEO_STORE_PATH = os.getenv('VIDEO_STORE_PATH')
+AUDIO_STORE_PATH = os.getenv('AUDIO_STORE_PATH')
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--audio", help="Download audio only", action="store_true")
+parser.add_argument("source", help="Youtube URL of the video to download")
+parser.add_argument("file_name", help="Final filename in local system")
 
 def progress_func(stream, bytes, bytes_remaining):
     total = stream.filesize
@@ -14,27 +21,29 @@ def progress_func(stream, bytes, bytes_remaining):
     end = '\r' if completed < 100 else '\n'
     print(f"Downloading... [{'#' * completed}{'.' * (100 - completed)}] {completed}%", end=end)
 
-def download_video(video_link: str, file_name: str) -> None:
+def download(audio, source, file_name):
     try:
-        yt = YouTube(video_link,
-                     on_progress_callback=progress_func,
-                    )
+        yt = YouTube(source, on_progress_callback=progress_func)
     except:
-        print('Connection Error')
+        print('Connection Error')    
         
-    video = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+    try:
+        if audio:
+            media = yt.streams.get_audio_only()
+            media.download(AUDIO_STORE_PATH, f'{file_name}.mp3')
+        else:
+            media = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+            media.download(VIDEO_STORE_PATH, f'{file_name}.mp4') 
+    except Exception as e:
+        print("There was an error during the download!", e) 
+    else:
+        print('Download completed successfully!')
+    
 
-    try: 
-        # downloading the video 
-        video.download(STORE_PATH, f'{file_name}.mp4') 
-    except Exception as e: 
-        print("There was an error downloading the video!", e) 
-    print('Download completed successfully!')
 
 def main() -> None:
-    video_link = sys.argv[1]
-    file_name = sys.argv[2]
-    download_video(video_link, file_name)
+    args = parser.parse_args()
+    download(args.audio, args.source, args.file_name)
 
 if __name__ == '__main__':
     main()
