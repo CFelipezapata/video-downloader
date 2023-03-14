7a268fc41922a2dd26728504ea91add44a52ca1c
FZapata <97852383+felipezapata01@users.noreply.github.com>
2023-03-13 22:01:59 -0500

    Move to setup.cfg file

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='VideoDownloader',
-      version='1.0',
-      description='Download videos from youtube',
-      author='Felipe Zapata',
-      packages=['downloader'],
-      install_requires=['pytube', 'python-dotenv'],
-      entry_points={
-          'console_scripts': ['vdownload = downloader.downloader:main']
-      }
-      )
+setup()
